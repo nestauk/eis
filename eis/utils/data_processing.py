18fa3cd39c2a4b2628513f82041ecc92721dbcca
Juan Mateos-Garcia <Juan.mateos-garcia@nesta.org.uk>
2020-09-08 12:39:40 +0100

    16 additional data (#18)
    * EDA of various indicators for final report
    
    * Updated notebooks

--- eis/utils/data_processing.py
+++ eis/utils/data_processing.py
@@ -1,4 +1,8 @@
 import matplotlib.pyplot as plt
+from altair_saver import save
+from dotenv import load_dotenv,find_dotenv
+from selenium import webdriver
+import os
 
 def flatten_list(a_list):
     return([x for el in a_list for x in el])
@@ -11,3 +15,11 @@ def save_fig(name,path,tight=True):
         
     plt.savefig(f"{path}/{name}")
     
+def save_altair(fig,name,w,fig_path,scale=3):
+    
+    save(fig,f"{fig_path}/{name}.png",method='selenium',webdriver=w,scale_factor=2)
+
+def make_altair_save():
+    load_dotenv(find_dotenv())
+    w = webdriver.Chrome(os.getenv('chrome_driver_path'))
+    return(w)
